b74b53e8eda2dff309e9aa4939280ab9440f3c80
zach2023 <132077542+zachmills2023@users.noreply.github.com>
2025-03-08 23:31:14 -0700

    Created MvcMovie App
    Created the app

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+        {
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "The Legend of Johnny Lingo",
+                    ReleaseDate = DateTime.Parse("2003-8-29"),
+                    Genre = "Family",
+                    Rating = "G",
+                    Price = 9.99M
+                },
+                new Movie
+                {
+                    Title = "The Other Side of Heaven",
+                    ReleaseDate = DateTime.Parse("2001-12-14"),
+                    Genre = "Adventure",
+                    Rating = "PG",
+                    Price = 12.99M
+                },
+                new Movie
+                {
+                    Title = "17 Miracles",
+                    ReleaseDate = DateTime.Parse("2011-6-3"),
+                    Genre = "History",
+                    Rating = "G",
+                    Price = 14.99M
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
